e5b0203368e0e0ba6b88d704182f48207b588bdd
Lam Kiều <huakieulam@gmail.com>
2024-03-24 13:47:04 +0700

    userroute, create models, controllers, .env.example

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config"
 import mongoose from 'mongoose';
+import UserRoute from './routes/UserRoute'; //router
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => console.log("connected"))
@@ -10,9 +11,11 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.get('/test', async (req, res) => {
-    res.json({ message: "Hello World" })
-});
+// app.get('/', async (req, res) => {
+//     res.json({ message: "Hi" })
+// });
+
+app.get('/api/user', UserRoute)
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
